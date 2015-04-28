4970eadbf40c2f038d097f3caebc3d5ea05707b9
John Wright <john@statemachinesystems.co.uk>
2015-04-28 15:16:21 +0100

    Add JavaDoc

--- src/main/java/com/statemachinesystems/util/DynamicReference.java
+++ src/main/java/com/statemachinesystems/util/DynamicReference.java
@@ -9,6 +9,11 @@ public class DynamicReference<T> {
 
     private final InheritableThreadLocal<T> threadLocal;
 
+    /**
+     * Create a dynamically-scoped reference with the given initial default value.
+     *
+     * @param initialValue  the initial default value to use
+     */
     public DynamicReference(T initialValue) {
         this.threadLocal = new InheritableThreadLocal<T>() {
             @Override
@@ -18,16 +23,27 @@ public class DynamicReference<T> {
         };
     }
 
+    /**
+     * Retrieve the current value of the reference.
+     *
+     * @return  the current value of the reference
+     */
     public T get() {
         return threadLocal.get();
     }
 
-    public void withValue(T newValue, Runnable thunk) {
+    /**
+     * Override the value of the reference while executing the given block.
+     *
+     * @param newValue  the overridden value to use within the given block
+     * @param block     the block to execute
+     */
+    public void withValue(T newValue, Runnable block) {
         T oldValue = threadLocal.get();
         threadLocal.set(newValue);
 
         try {
-            thunk.run();
+            block.run();
         } finally {
             threadLocal.set(oldValue);
         }
